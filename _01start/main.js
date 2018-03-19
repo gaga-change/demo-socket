b42bf8a78732cc206e28838cffad9ac90e8878be
严俊东 <1172482914@qq.com>
2018-03-19 21:26:35 +0800

    简易通信

--- _01start/main.js
+++ _01start/main.js
@@ -10,12 +10,14 @@ const io = socketIo(server)
 app.use('/', express.static(path.resolve(__dirname, 'public')))
 
 io.on('connection', socket => {
-    console.log('a user connect')
+    socket.on('new message', (data) => {
+        socket.broadcast.emit('new message', data)
+    })
     socket.on('disconnect', function () {
         console.log('user disconnected')
     })
 })
 
-server.listen(3000, () => {
-    console.log(3000)
+server.listen(3001, () => {
+    console.log(3001)
 })
--- _01start/public/main.js
+++ _01start/public/main.js
@@ -1,1 +1,16 @@
 var socket = io()
+
+$('#commitBtn').click(function () {
+    var msg = $('#message').val()
+    $('#message').val('')
+    socket.emit('new message', msg)
+    addMsg(msg)
+})
+
+socket.on('new message', function (msg) {
+    addMsg(msg)
+})
+
+function addMsg(msg) {
+    $('#contain').append($('<p></p>').text(msg))
+}
