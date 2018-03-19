b0b8ffcd560dc2ada5224905fb56edd894b754e3
严俊东 <1172482914@qq.com>
2018-03-19 20:51:52 +0800

    start

--- _01start/main.js
+++ _01start/main.js
@@ -4,7 +4,8 @@ const http = require('http')
 const path = require('path')
 
 const app = express()
-const io = socketIo(http.Server(app))
+const server = http.Server(app)
+const io = socketIo(server)
 
 app.use('/', express.static(path.resolve(__dirname, 'public')))
 
@@ -15,24 +16,6 @@ io.on('connection', socket => {
     })
 })
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log(3000)
 })
-
-
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
-
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
-    
